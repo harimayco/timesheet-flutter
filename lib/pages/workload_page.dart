3b85ee0d1f267474d4caa215eeee92941badc2c2
Rendy Harimayco <rendyharimayco@gmail.com>
2019-07-20 16:33:51 +0700

    Clean up

--- lib/pages/workload_page.dart
+++ lib/pages/workload_page.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class WorkloadPage extends StatelessWidget {
+  final workloadTeam = [
+    "Anastasia Jeannet",
+    "Budi St",
+    "Rendy Harimayco",
+    "Stefanus Eka",
+    "Ryan Linardi"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -22,6 +31,52 @@ class WorkloadPage extends StatelessWidget {
         ),
         body: TabBarView(
           children: <Widget>[
+            ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Padding(
+                    padding: EdgeInsets.all(15),
+                    child: ExpansionTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            "https://s3.amazonaws.com/uifaces/faces/twitter/marcoramires/128.jpg"),
+                      ),
+                      title: Text(
+                        workloadTeam[index],
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 18),
+                      ),
+                      backgroundColor: Colors.white,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(top: 20),
+                        ),
+                        CustomExpandedItem(
+                          title: "Carrol Company: Virtuemart Plugin",
+                          dueDate: "2018-03-19",
+                        ),
+                        CustomExpandedItem(
+                          title: "Latinnews: Add new feature to admin page",
+                          dueDate: "2019-04-17",
+                        ),
+                        CustomExpandedItem(
+                          title: "Trabolgan: Email issue",
+                          dueDate: "2017-05-05",
+                        ),
+                        CustomExpandedItem(
+                          title:
+                              "Aran Sweater Market: woocommerce & site Performance Issue",
+                          dueDate: "2016-07-07",
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+              itemCount: workloadTeam.length,
+            ),
             ListView(
               shrinkWrap: true,
               children: <Widget>[
@@ -159,10 +214,65 @@ class WorkloadPage extends StatelessWidget {
             ),
             Text("Test2"),
             Text("test3"),
-            Text("Test4")
           ],
         ),
       ),
+    );
+  }
+}
+
+class CustomExpandedItem extends StatelessWidget {
+  final String title;
+  final String dueDate;
+
+  CustomExpandedItem({this.title, this.dueDate});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(top: 5),
+          child: Icon(
+            Icons.arrow_forward_ios,
+            size: 16,
+          ),
+          margin: EdgeInsets.only(right: 10),
+        ),
+        Flexible(
+          child: Container(
+            //color: Colors.red,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 16,
+                    //fontWeight: FontWeight.w500,
+                  ),
+                  maxLines: 2,
+                  softWrap: true,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 3),
+                ),
+                Text(
+                  "Due Date: $dueDate",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10),
+                )
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
